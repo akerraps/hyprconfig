d064b87e3ee07621a3e6bbd782f1bf815974e8cc
akerraps <akerraps@gmail.com>
2025-09-26 12:20:25 +0200

    Add harpoon plugin to nvim

---
+++ nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,93 @@
+return {
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+
+  config = function()
+    local harpoon = require("harpoon")
+    harpoon:setup()
+  end,
+
+  keys = {
+    {
+      "<leader>a",
+      function()
+        require("harpoon"):list():add()
+      end,
+      desc = "Harpoon add file",
+    },
+
+    {
+      "<C-1>",
+      function()
+        require("harpoon"):list():select(1)
+      end,
+      desc = "Harpoon file 1",
+    },
+    {
+      "<C-2t>",
+      function()
+        require("harpoon"):list():select(2)
+      end,
+      desc = "Harpoon file 2",
+    },
+    {
+      "<C-3>",
+      function()
+        require("harpoon"):list():select(3)
+      end,
+      desc = "Harpoon file 3",
+    },
+    {
+      "<C-4>",
+      function()
+        require("harpoon"):list():select(4)
+      end,
+      desc = "Harpoon file 4",
+    },
+
+    {
+      "<C-S-P>",
+      function()
+        require("harpoon"):list():prev()
+      end,
+      desc = "Harpoon prev file",
+    },
+    {
+      "<C-S-N>",
+      function()
+        require("harpoon"):list():next()
+      end,
+      desc = "Harpoon next file",
+    },
+
+    {
+      "<C-e>",
+      function()
+        local harpoon = require("harpoon")
+        local conf = require("telescope.config").values
+
+        local function toggle_telescope(harpoon_files)
+          local file_paths = {}
+          for _, item in ipairs(harpoon_files.items) do
+            table.insert(file_paths, item.value)
+          end
+
+          require("telescope.pickers")
+            .new({}, {
+              prompt_title = "Harpoon",
+              finder = require("telescope.finders").new_table({
+                results = file_paths,
+              }),
+              previewer = conf.file_previewer({}),
+              sorter = conf.generic_sorter({}),
+            })
+            :find()
+        end
+
+        toggle_telescope(harpoon:list())
+      end,
+      desc = "Harpoon Telescope menu",
+    },
+  },
+}
